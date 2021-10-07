8ea928c82b593a08551676ceff17f51ba70d7147
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-07 20:40:32 +0530

    Added: Employee present or absent

--- src/main/java/com/bdlz/empwageoop/EmployeeWage.java
+++ src/main/java/com/bdlz/empwageoop/EmployeeWage.java
@@ -2,6 +2,11 @@ package com.bdlz.empwageoop;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage program");
-    }
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }    }
 }
